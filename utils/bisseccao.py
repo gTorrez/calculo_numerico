81125f7a1385fe1afe650993004148559f117908
gTorrez <g.ctorres09@gmail.com>
2023-02-21 23:17:12 -0300

    fix pequenas alteracoes no metodo da bisseccao

--- utils/bisseccao.py
+++ utils/bisseccao.py
@@ -1,16 +1,25 @@
-from math import sin
-
+r = 0.401
+s = 42.7*(10**(-6))
+n_molec = 1000
+t = 300
+p = 3.5*(10**7)
+k = 1.3806503 * (10**(-23))
 
 def biseccao(a, b, tolerancia=0.00000001):
-    m_list = []
     i = 0
 
     print("Método da bissecção\n")
-    while True:
-        fa = (a/2)**2 - sin(a)
+    while abs(b-a)/2 > tolerancia:
+        fa = (p + r * (n_molec/a)**2)*(a-n_molec*s)*(1/(k*n_molec*t))
+        fb = (p + r * (n_molec/b)**2)*(b-n_molec*s)*(1/(k*n_molec*t))
+        if fa*fb >= 0 and i == 0:
+            print("Intervalo inválido pois não satisfaz f(a)f(b) < 0")
+            return
 
         m = (a+b)/2
-        fm = (m/2)**2 - sin(m)
+        fm = (p + r * (n_molec/m)**2)*(m-n_molec*s)*(1/(k*n_molec*t))
+        if fm == 0:
+            break
 
         if fa*fm < 0:
             a = a
@@ -19,17 +28,9 @@ def biseccao(a, b, tolerancia=0.00000001):
         else:
             a = m
             b = b
-
         i += 1
-
-        m_list.append(m)
-        if len(m_list) > 2:
-            m_list.pop(0)
-
-        if (len(m_list) == 2) and abs(m_list[1] - m_list[0]) <= tolerancia:
-            break
 
     print(f"Número de iterações: {i}, Valor encontrado = {m:.10f}")
 
 
-biseccao(1.5, 2)
+biseccao(0.00001, 1)
